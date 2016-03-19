285b11a468eec1ffc11a3f13cc7e25f41d4f9841
Soumya Ray <soumya.ray@gmail.com>
2016-03-19 14:28:35 +0800

    Rename and update openss_aes demo

--- openssl_aes.rb
+++ openssl_aes.rb
@@ -3,7 +3,7 @@ def puts_bytes(description, str_utf8)
   puts "#{description} (ascii): #{str_utf8.inspect}"
   padding = ' ' * (description.length + 3)
   puts padding + "(dec): #{str_utf8.bytes.join(' ')}"
-  puts padding + "(hex): #{str_utf8.unpack("H*").first}"
+  puts padding + "(hex): #{str_utf8.unpack('H*').first}"
   puts padding + "(b64): #{Base64.strict_encode64(str_utf8)}"
   puts
 end
@@ -24,8 +24,7 @@ def aes_demo(plaintext)
   ciphertext = cipher.update(plaintext) + cipher.final
   puts_bytes('ENCRYPTED', ciphertext)
 
-  decipher = OpenSSL::Cipher::AES.new(128, :CBC)
-  decipher.decrypt
+  decipher = OpenSSL::Cipher::AES.new(128, :CBC).decrypt
   decipher.key = key
   decipher.iv = iv
 
@@ -35,33 +34,35 @@ end
 
 aes_demo('electric sheep')
 
-# Examing the elements of OpenSSL's AES cipher
+# Examining the elements of OpenSSL's AES cipher
 plaintext = ['Do ', 'androids ', 'dream ', 'of ', 'electric ', 'sheep?']
 cipher = OpenSSL::Cipher::AES.new(128, :CBC).encrypt
 key = cipher.random_key
 iv = cipher.random_iv
-encrypted = (plaintext.map { |plain| cipher.update(plain) }) << cipher.final
+
+crypt_blocks = plaintext.map do |plain|
+  cipher.update(plain)
+end
+encrypted = crypt_blocks << cipher.final
 ciphertext = encrypted.join
 
 decipher = OpenSSL::Cipher::AES.new(128, :CBC).decrypt
 decipher.key = key
 decipher.iv = iv
+
+# Decrypt the whole thing:
 decrypted = decipher.update(ciphertext) + decipher.final
 
-
-
-# QUESTION: Will the following code decrypt our encrypted array?
-#     hint: are the encrypted and decrypted bytes in the same order?
+# Decrypt array piecewise:
 decrypted = encrypted.map do |secret|
-  decipher.update(secret)
+  secret.empty? ? '' : decipher.update(secret)
 end << decipher.final
 
-
-
+puts decrypted
 
 ## NOTES: making a base64 string with non-alphanumeric characters
 loop do
- data = (1..5).map { rand(0..2222).chr(Encoding::UTF_8) }.join
- str = Base64.strict_encode64(data)
- break if %w(+ / =).all? { |c| str.include? c }
+  data = (1..5).map { rand(0..2222).chr(Encoding::UTF_8) }.join
+  str = Base64.strict_encode64(data)
+  break if %w(+ / =).all? { |c| str.include? c }
 end
