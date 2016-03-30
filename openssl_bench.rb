23985e09f9c970025b89d1c65da302fc25fd6d65
Soumya Ray <soumya.ray@gmail.com>
2016-03-30 23:02:53 +0800

    Create openssl_rsa and openssl_bench scripts

---
+++ openssl_bench.rb
@@ -0,0 +1,51 @@
+require 'openssl'
+require 'benchmark'
+
+# Setup basic parameters
+plaintext = 'Hello darkness my old friend'
+n = 10_000
+
+# Setup AES ciphers
+aes_cipher = OpenSSL::Cipher::AES.new(128, :CBC).encrypt
+key = aes_cipher.random_key
+iv = aes_cipher.random_iv
+aes_ciphertext = aes_cipher.update(plaintext) + aes_cipher.final
+
+aes_decipher = OpenSSL::Cipher::AES.new(128, :CBC).decrypt
+aes_decipher.key = key
+aes_decipher.iv = iv
+
+# Setup RSA ciphers
+rsa_private = OpenSSL::PKey::RSA.generate 2048
+rsa_ciphertext = rsa_private.public_encrypt plaintext
+rsa_public = rsa_private.public_key
+
+# Benchmark AES vs. RSA, encrypt vs. decrypt
+results = Benchmark.bm(15) do |bench|
+  bench.report('aes encrypt') do
+    n.times do
+      aes_cipher.update(plaintext) + aes_cipher.final
+    end
+  end
+
+  bench.report('aes decrypt') do
+    n.times do
+      aes_decipher.update(aes_ciphertext) + aes_decipher.final
+    end
+  end
+
+  bench.report('rsa encrypt') do
+    n.times do
+      rsa_public.public_encrypt plaintext
+    end
+  end
+
+  bench.report('rsa decrypt') do
+    n.times do
+      rsa_private.private_decrypt rsa_ciphertext
+    end
+  end
+end
+
+results[2].utime / results[0].utime
+results[3].utime / results[1].utime
---
+++ openssl_rsa.rb
@@ -0,0 +1,24 @@
+require 'openssl'
+
+# see: ruby-doc.org/stdlib-2.0/libdoc/openssl/rdoc/OpenSSL.html
+
+# RSA ciphers
+rsa_private = OpenSSL::PKey::RSA.generate 2048
+rsa_private.to_pem
+
+rsa_public = rsa_private.public_key
+rsa_public.to_pem
+
+# RSA Encryption and decryption
+plaintext = 'Hello darkness my old friend'
+
+rsa_ciphertext = rsa_public.public_encrypt plaintext
+puts rsa_private.private_decrypt(rsa_ciphertext)
+
+# Limits of plaintext size
+def random_str(n = 10)
+  letters = ('a'..'z').to_a + ('A'..'Z').to_a
+  (1..n).map { letters.sample }.join
+end
+
+rsa_public.public_encrypt random_str(500)
