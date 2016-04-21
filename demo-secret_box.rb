eb5887e5b2cfbd2080404c5e62cabe9e02698a9b
Soumya Ray <soumya.ray@gmail.com>
2016-04-21 16:18:54 +0800

    demo-secretbox: reformat output

--- demo-secret_box.rb
+++ demo-secret_box.rb
@@ -1,45 +1,43 @@
 require 'rbnacl/libsodium'
 
-## Encryption
-
-key = RbNaCl::Random.random_bytes(RbNaCl::SecretBox.key_bytes)
-# => "\xE3\xEB\xDDh?\xEC\x06\xF3>\xD3o\x14t\x94\xE3h\xC4\xAD\xCE\xB6PgJ0\x99Q\xAC<\xA1\xA4\x9E\x9D"
-
-secret_box = RbNaCl::SecretBox.new(key)
-# => #<RbNaCl::SecretBoxes::XSalsa20Poly1305:0x007f91224ed710
-#      @key="\xE3\xEB\xDDh?\xEC\x06\xF3>\xD3o\x14t\x94\xE3h\xC4\xAD\xCE\xB6PgJ0\x99Q\xAC<\xA1\xA4\x9E\x9D">
-
-nonce = RbNaCl::Random.random_bytes(secret_box.nonce_bytes)
-# => "\x13\xCE\xD0\xD0\xB7j2\ac\x1ES\x9E\x97\x84o\x9C\x11/\x1D\xD0\x83\xF1g\x91"
-
+## SecretBox Encryption
 message = 'This is my secret sauce'
 # => "This is my secret sauce"
 
+key = RbNaCl::Random.random_bytes(RbNaCl::SecretBox.key_bytes)
+key.bytes
+# => [2, 160, 76, 186, 1, 16, 157, 214, 114, 167, 214, 105, 189, 77, 218, 222,
+#     198, 23, 143, 129, 214, 146, 79, 13, 181, 91, 0, 45, 45, 106, 47, 198]
+
+secret_box = RbNaCl::SecretBox.new(key)
+nonce = RbNaCl::Random.random_bytes(secret_box.nonce_bytes)
+nonce.bytes
+# => [111, 109, 99, 174, 177, 86, 173, 71, 166, 199, 100, 19, 12, 218, 111, 173,
+#     39, 44, 138, 205, 3, 180, 56, 90]
+
 ciphertext = secret_box.encrypt(nonce, message)
-# => "n\xA8NX%\xC6tkzf8\b\xC9\xC7}\xCD\x9A\x93n\x92\xF3cGmD\xB8bN \xF0\x8C\xE2D+(\xE9\xEF\xEC\xDB"
+ciphertext.bytes
+# => [90, 231, 138, 178, 38, 60, 103, 91, 99, 91, 214, 30, 90, 235, 183, 161,
+#     38, 1, 2, 234, 117, 106, 119, 215, 136, 117, 177, 141, 48, 93, 175, 147,
+#     19, 128, 51, 56, 118, 67, 188]
 
 
-## Decryption
-
-# retrieve key and nonce
+## Decryption with SecretBox
+# retrieve earlier key and nonce
 # key = ...
 # nonce = ...
 
 secret_box = RbNaCl::SecretBox.new(key)
-# => #<RbNaCl::SecretBoxes::XSalsa20Poly1305:0x007f912227ee20
-#      @key="\xE3\xEB\xDDh?\xEC\x06\xF3>\xD3o\x14t\x94\xE3h\xC4\xAD\xCE\xB6PgJ0\x99Q\xAC<\xA1\xA4\x9E\x9D">
-
 decrypted_message = secret_box.decrypt(nonce, ciphertext)
 # => "This is my secret sauce"
 
 
 ## Tampering with the ciphertext raises an exception
-
 ciphertext[5] = '?'
 # => "?"
 
 ciphertext
-# => "n\xA8NX%?tkzf8\b\xC9\xC7}\xCD\x9A\x93n\x92\xF3cGmD\xB8bN \xF0\x8C\xE2D+(\xE9\xEF\xEC\xDB"
+# => "n\xA8NX%?tkzf8\b\xC9\xC7}\xCD\x9A\x93n\x92\xF3cGmD\xB8bN..."
 
 decrypted_message = desecret_box.decrypt(nonce, ciphertext)
 # RbNaCl::CryptoError: Decryption failed. Ciphertext failed verification.
