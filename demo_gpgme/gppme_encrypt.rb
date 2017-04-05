50e4b46e1a013011c4c6cc71eb5f48cc3c20bde1
Soumya Ray <soumya.ray@gmail.com>
2017-04-06 00:04:48 +0800

    Add decryption to GPGME demo code

--- demo_gpgme/gppme_encrypt.rb
+++ demo_gpgme/gppme_encrypt.rb
@@ -1,8 +1,16 @@
 require 'gpgme'
 
-key = File.read('CE510594.asc')
-GPGME::Key.import key
+message = 'testing testing'
 
+key = File.read('CE510594.asc');
+GPGME::Key.import key
 crypto = GPGME::Crypto.new always_trust: true, armor: true
-result = crypto.encrypt 'testing testing', recipients: 'soumya.ray@gmail.com'
-puts result.to_s
+ciphertext = crypto.encrypt message, recipients: 'soumya.ray@gmail.com'
+# => #<GPGME::Data:0x007fe753c27b48>
+puts ciphertext
+
+plaintext = crypto.decrypt ciphertext
+plaintext
+# => #<GPGME::Data:0x007fe755b69ba0>
+puts plaintext
+# testing testing
