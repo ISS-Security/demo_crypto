df4969450f8ead33ad7dac37182cf58c98b668a3
Soumya Ray <soumya.ray@gmail.com>
2018-05-03 13:45:41 +0800

    update password hashing code

--- rbnacl/rbnacl-password_hash.rb
+++ rbnacl/rbnacl-password_hash.rb
@@ -2,15 +2,16 @@
 require 'rbnacl/libsodium'
 require 'base64'
 
-password = 'cantguessthis'
-sameword = 'cantguessthis'
+password1 = 'cantguessthis'
+password2 = 'cantguessthis'
+
 def only_hash(password)
   Base64.strict_encode64 RbNaCl::Hash.sha256(password)
 end
 
-only_hash(password)
+only_hash(password1)
 # => "1h/i4qlz3vZmKdhdYgN0SEO1gRI7O7Jmo7CAMZG5TJU="
-only_hash(sameword)
+only_hash(password2)
 # => "1h/i4qlz3vZmKdhdYgN0SEO1gRI7O7Jmo7CAMZG5TJU="
 
 def salted_hash(password)
@@ -18,10 +19,10 @@ def salted_hash(password)
   Base64.strict_encode64 RbNaCl::Hash.sha256(password + salt)
 end
 
-salted_hash(password)
-# => "Ja0leWiUk4piW43r5tqFkb25FU5cGxcuwnJhRjCgiSc="
-salted_hash(sameword)
-# => "fNzWxHtVohPfKF7+7AoRqkIaBp6xQ/bO2UVVJi3hn+Q="
+salted_hash(password1)
+# => "r4+1J4wJDDrb6FmvF6MqmUl3PX2ldJA5TwYD9g08agI="
+salted_hash(password2)
+# => "nY3ENXOjHJsP4+FwhaHEXWH73Me7iPnNodDkzUDwT38="
 
 def hard_hash(password)
   opslimit = 2**20
@@ -36,26 +37,18 @@ def hard_hash(password)
   [Base64.strict_encode64(digest), Base64.strict_encode64(salt)].join("\n")
 end
 
-hard_hash(password)
-# => "dX7g2Qf+3cS8eXq7VCBPpvKr076IpFpOr+O6D5tdMMCdpuyHSBgds5JPbc2FbMLl7MFZ+b0WJU6tWWign4hJTw==\n" +
-# "bXhGWTZMUWV2Qm8ra0wybmZBY0JvWGtLMzJzcDNsczhmVXpzdnZWbFBaVT0="
-hard_hash(sameword)
-# => "MnPItHLr+UMzDk5pTkS8GEacQZdWa0NoWFdeEimMSa22U7KqII/3noeZO0r3YqDkdDt+WGYXBUGL0efHD0qFuA==\n" +
-# "OW9PdTRWc0Rxb3UzT3doU0hOZmFhUVRxTlVmZ1RhdVFXYld4aVUzL3lTWT0="
+hard_hash(password1)
+hard_hash(password2)
 
 require 'benchmark'
 
 Benchmark.bm do |bench|
-  n = 1000
+  n = 100
   bench.report('sha256') do
-    n.times { salted_hash(password) }
+    n.times { salted_hash(password1) }
   end
 
   bench.report('scrypt') do
-    n.times { hard_hash(password) }
+    n.times { hard_hash(password1) }
   end
 end;
-
-#            user     system      total        real
-# sha256  0.010000   0.000000   0.010000 (  0.006641)
-# scrypt 50.470000   5.620000  56.090000 ( 56.143286)
