710cd53593bfd6c481ec6d3eaa9df9473ebdc94b
Soumya Ray <soumya.ray@gmail.com>
2021-03-19 09:02:38 +0800

    rename vars

--- one_time_pad.rb
+++ one_time_pad.rb
@@ -1,14 +1,14 @@
 # Demonstrating the One-Time Pad
 def one_time_pad(plain_ints, key_ints)
-  xor_pairs = plain_ints.zip key_ints
-  xor_pairs.map { |p, k| p ^ k }
+  int_pairs = plain_ints.zip key_ints
+  int_pairs.map { |p, k| p ^ k }
 end
 
 plain = 'bad'.bytes
 key   = 'hog'.bytes
-cipher_b = one_time_pad(plain, key)
+cipher_ints = one_time_pad(plain, key)
 
-one_time_pad(cipher_b, key)
+one_time_pad(cipher_ints, key)
 
 ## DANGER IN REUSING KEYS
 p1 = 'tin'.bytes
@@ -24,5 +24,5 @@ c3 = one_time_pad(p3, key)
 p1_guess = 'tin'.bytes
 
 # Can we extract plaintext _without_ the key?
-one_time_pad(one_time_pad(c1, c2), p1_guess)
-one_time_pad(one_time_pad(c1, c3), p1_guess)
+one_time_pad(one_time_pad(c1, c2), p1_guess).map(&:chr).join
+one_time_pad(one_time_pad(c1, c3), p1_guess).map(&:chr).join
