82b694248007b103664774953a74115cb93cc2c4
Soumya Ray <soumya.ray@gmail.com>
2024-03-21 22:41:26 +0800

    Update AES demo

--- demo-aes.rb
+++ demo-aes.rb
@@ -22,15 +22,6 @@ def aes_decrypt(key, ciphertext)
   cipher.update(ciphertext) + cipher.final
 end
 
-# symkey = aes_key
-# c1 = aes_encrypt(symkey, 'electric sheep')
-# c2 = aes_encrypt(symkey, 'nosferetu lie')
-
-# def str_xor(str1, str2)
-#   bytezip = str1.bytes.zip(str2.bytes)
-#   xored = bytezip.map { |b1, b2| b1 ^ b2 }
-#   xored.map(&:chr).join
-# end
-
-# c1_xor_c2 = str_xor(c1, c2)
-# str_xor('nosferetu lie', c1_xor_c2)
+symkey = aes_key
+c1 = aes_encrypt(symkey, 'electric sheep')
+aes_decrypt(symkey, c1) # => "electric sheep"
